f82749014bb1369524d2451f9908f93523add1dd
Defolto <37343435+Defolto@users.noreply.github.com>
2019-08-18 10:56:06 +0300

    animation

--- script.js
+++ script.js
@@ -61,3 +61,46 @@ function timer() {
     setTimeout(timer, 150);
 }  
 timer();
+
+let b = 2;
+$('.About > div div p').hide();
+function site(){
+    if(b != 5){
+        $(`.About:nth-child(${b}) > div div p`).show().addClass('flipInX');
+        b++;
+    }
+    else{
+        return;
+    }
+    setTimeout(site, 300);
+}
+setTimeout(site, 500);
+
+var target1 = $('#target1');
+var target2 = $('#target2');
+var target3 = $('#target3');
+
+var targetPos1 = target1.offset().top;
+var winHeight1 = $(window).height();
+var scrollToElem1 = targetPos1 - winHeight1;
+var targetPos2 = target2.offset().top;
+var winHeight2 = $(window).height();
+var scrollToElem2 = targetPos2 - winHeight2;
+var targetPos3 = target3.offset().top;
+var winHeight3 = $(window).height();
+var scrollToElem3 = targetPos3 - winHeight3;
+$(window).scroll(function(){
+    var winScrollTop1 = $(this).scrollTop();
+    if(winScrollTop1 > scrollToElem1){
+        $(".left").show().addClass('fadeInLeft');
+        $(".right").show().addClass('fadeInRight');
+    }
+    var winScrollTop2 = $(this).scrollTop();
+    if(winScrollTop2 > scrollToElem2){
+        $(".foto").show().addClass('fadeInRight');
+    }
+    var winScrollTop3 = $(this).scrollTop();
+    if(winScrollTop3 > scrollToElem3){
+        $(".forma").show().addClass('fadeInUpBig');
+    }
+});
